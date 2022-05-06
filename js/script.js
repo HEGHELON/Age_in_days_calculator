26fbe41e2559b5583650407562dff11b389de94a
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-05-07 00:35:53 +0100

    edited html, style and script

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 function AgeInDays() {
-  let birthyear = prompt('What is yor birthyear?');
-  let birthday = (2022-birthyear)*365;
-  console.log(birthday);
+  let birthyear = prompt("What is yor birthyear?");
+  let birthday = (2022 - birthyear) * 365;
+  console.log(birthyear);
+  let test = /[0-9]{4}|\d+/.test(birthyear);
+  if (!test && birthyear !== " ") {
+    alert("Please enter a valid year");
+  } else {
+    if (parseInt(birthyear) > 2022) {
+      alert("Please enter a valid year");
+    } else {
+      console.log(/[0-9]{4}/.test(birthyear));
 
-  let h1 = document.createElement("h1")
-  let outputText = document.createTextNode("You are " + birthday + " days old")
-  h1.setAttribute('id', 'birthdays')
-  h1.appendChild(outputText);
-  document.getElementById('output').appendChild(h1);
-} 
+      let h3 = document.createElement("h3");
+      let outputText = document.createTextNode(
+        "You are " + birthday + " days old"
+      );
+      h3.setAttribute("id", "birthdays");
+      h3.appendChild(outputText);
+      document.getElementById("output").appendChild(h3);
+      // }
+    }
+  }
+}
 
 function Reset() {
- document.getElementById('birthdays').remove();
- 
+  document.getElementById("birthdays").remove();
 }
